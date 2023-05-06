07feab0defeb25ce20d55d805c58c73bcf890db0
jcebo <56255697+jcebo@users.noreply.github.com>
2023-05-07 01:38:09 +0200

    sign-in and -up added

--- note-spa/src/App.vue
+++ note-spa/src/App.vue
@@ -1,30 +1,61 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+
+
+    <div class="container">
+      <h1>
+        <span style="color: black"
+          >NoteApp
+          <img style="height: 40px" src="./assets/note.svg" alt="NoteAppLogo" />
+        </span>
+        <button v-if="isLoggedIn" class="btn right" @click="handleLogOut">
+          Log-out
+        </button>
+      </h1>
+          <router-view />
+
+    </div>
+
+  
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+
+<script setup>
+import { onMounted, ref } from "vue";
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+import { useRouter } from "vue-router";
+
+const isLoggedIn = ref(false);
+const router = useRouter();
+let auth;
+
+onMounted(() => {
+  auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      isLoggedIn.value = true;
+    } else {
+      isLoggedIn.value = false;
+    }
+  });
+});
+// eslint-disable-next-line no-unused-vars
+const handleLogOut = () => {
+  signOut(auth).then(() => {
+    router.push("sign-in");
+  });
+};
+</script>
+
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
---
+++ note-spa/src/router/index.js
@@ -0,0 +1,48 @@
+import { createRouter, createWebHistory } from "vue-router";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+// import bootstrap from "bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: "/", component: () => import("../views/SignIn.vue") },
+        { path: "/register", component: () => import("../views/SignUp.vue") },
+        { path: "/sign-in", component: () => import("../views/SignIn.vue") },       
+        
+        {
+            path: "/home", component: () => import("../views/Home.vue"), meta: {
+                requiresAuth: true,
+            }
+        },                     
+    ],
+});
+
+const getCurrentUser = () => {
+    return new Promise((resolve, reject) => {
+        const removeListener = onAuthStateChanged(
+            getAuth(),
+            (user) => {
+                removeListener();
+                resolve(user);
+            },
+            reject
+        )
+    })
+}
+
+router.beforeEach(async (to, from, next) => {
+    if (to.matched.some((record) => record.meta.requiresAuth)) {
+        if (await getCurrentUser()) {
+            next()
+        } else {
+            next("/");
+            alert("no access");
+        }
+
+    } else {
+        next();
+    }
+});
+export default router;
