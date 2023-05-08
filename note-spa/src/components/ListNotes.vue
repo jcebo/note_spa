3875b326e4d05d6e9f725fe407c70732591b9ece
Rose-Mar <57239263+Rose-Mar@users.noreply.github.com>
2023-05-09 00:36:54 +0200

    btn

--- note-spa/src/components/ListNotes.vue
+++ note-spa/src/components/ListNotes.vue
@@ -1,64 +1,91 @@
 <script>
 import { collection, getDocs } from "firebase/firestore"; 
-import {db} from "../main.js";
-import {getAuth} from "firebase/auth";
+import { db } from "../main.js";
+import { getAuth } from "firebase/auth";
 import { getStorage, ref, listAll, getDownloadURL } from "firebase/storage";
 
-
 export default {
-    data() {
-        return {
-            notesList: [],
-            text_notes: [],
-        }
+  data() {
+    return {
+      notesList: [],
+      text_notes: [],
+      showImages: true,
+      showText: true,
+      showRecordings: true,
+    };
+  },
+  async mounted() {
+    console.log("show notes");
+    const auth = getAuth();
+
+    const querySnapshot = await getDocs(
+      collection(db, `users/${auth.currentUser.uid}/notes`)
+    );
+
+    querySnapshot.forEach(async (doc) => {
+      console.log(`${doc.id} => ${doc.data()}`);
+      const noteData = doc.data();
+      if (noteData.ImageURL) {
+        const storageRef = ref(getStorage(), noteData.ImageURL);
+        const imageURL = await getDownloadURL(storageRef);
+        noteData.ImageURL = imageURL;
+      }
+      this.text_notes.push(noteData);
+
+      console.log("noteData:");
+      console.log(noteData);
+    });
+    console.log("end of showing notes");
+    //console.log(data)
+    console.log("????????????????????????????");
+
+    this.displayedNotes = this.text_notes;
+  },
+  methods: {
+    first20: function (string) {
+      let split_string = string.split(" ");
+      let result = split_string.slice(0, 20).join(" ");
+      if (split_string.length > 20) {
+        result = result + " ...";
+      }
+
+      console.log("first20 split result" + result);
+      return result;
     },
-    async mounted() {
-        console.log("show notes")
-        const auth = getAuth();
-
-        const querySnapshot = await getDocs(collection(db, `users/${auth.currentUser.uid}/notes`));
-        
-        querySnapshot.forEach(async (doc) => {
-            console.log(`${doc.id} => ${doc.data()}`);
-            const noteData = doc.data();
-            if (noteData.ImageURL) {
-              const storageRef = ref(getStorage(), noteData.ImageURL);
-              const imageURL = await getDownloadURL(storageRef);
-              noteData.ImageURL = imageURL;
-            }
-            this.text_notes.push(noteData);
-
-            console.log("noteData:")
-            console.log(noteData)
-
-        });
-        console.log("end of showing notes")
-        //console.log(data)
-        console.log("????????????????????????????")
+  },
+  computed: {
+    filteredNotes() {
+      return this.text_notes.filter((note) => {
+        return (
+          (note.ImageURL && this.showImages) ||
+          (!note.ImageURL && this.showText) ||
+          (note.RecordingURL && this.showRecordings)
+        );
+      });
     },
-    methods: {
-        first20: function (string) {
-            let split_string = string.split(' ');
-            let result = split_string.slice(0, 20).join(' ')
-            if (split_string.length > 20) {
-                result = result + ' ...'
-            }
-
-            console.log("first20 split result" + result)
-            return result
-        },
-    }
-}
+  },
+};
 </script>
+
+
 
 <template>
     <div class="mx-auto">
-        <div class="container-flex" v-if="text_notes">
+        <div class="container-flex">
+            <div class="d-flex justify-content-end mb-2">
+            <button class="btn btn-primary me-2" @click="showImages = !showImages">
+            {{ showImages ? "Ukryj notatki z obrazkami" : "Pokaż notatki z obrazkami" }}
+            </button>
+            <button class="btn btn-primary me-2" @click="showText = !showText">
+            {{ showText ? "Ukryj notatki z tekstem" : "Pokaż notatki z tekstem" }}
+            </button>
+            <button class="btn btn-primary" @click="showRecordings = !showRecordings">
+            {{ showRecordings ? "Ukryj notatki z nagraniami" : "Pokaż notatki z nagraniami" }}
+            </button>
+        </div>
             <TransitionGroup name="list">
-                <div class="card mb-1" v-for="note in text_notes" :key="note.Title">
+                <div class="card mb-1" v-for="note in filteredNotes" :key="note.Title">
                     <router-link to="{name:'note', params:{ id: note.id}}">
-                    <!-- <router-link to="{name:'note'}" params="{ id: note.id}"> -->
-                    <!-- <router-link :to="{name:'note', params:{ id: note.id}}"></router-link> -->
                         <div class="card-body" style="text-align: justify;text-justify: inter-word;">
                             <h5 class="card-title">{{ note.Title }}</h5>
                             <img v-if="note.ImageURL" :src="note.ImageURL" alt="Note Image">
